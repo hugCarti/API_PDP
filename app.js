464ef4da6bc960f2f4e5c3bf65a61e5ae5843985
HugCarti <Hleplingard04@gmail.com>
2025-06-11 22:45:42 +0200

    merge branch second

--- app.js
+++ app.js
@@ -2,22 +2,27 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
 const catwayRoutes = require('./routes/catwayRoutes');
 const privateRoutes = require('./routes/privateRoutes');
 
 const app = express();
 
-// Configuration des vues
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// Middlewares
 app.use(cors());
+app.use(cookieParser());
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
+<<<<<<< HEAD
 // Routes
+=======
+>>>>>>> second
 app.use('/', privateRoutes);
 app.use('/', catwayRoutes);
 
